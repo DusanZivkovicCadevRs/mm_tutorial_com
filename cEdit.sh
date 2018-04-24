c67ae348e72ace3f43ced6c368433897f4bd3691
Dušan Živković <dzivkovic@comtrade.com>
2018-04-24 10:53:47 +0200

    curls sredjeni

--- cEdit.sh
+++ cEdit.sh
@@ -1,1 +1,6 @@
-curl -H "Content-Type: application/json" -X PUT -d '{"status":"complete"}' localhost:3000/todos/5adde33fe9218b0a8cc8809f
+curl -X PUT \
+ localhost:3000/todos/5adde33fe9218b0a8cc8809f \
+ -H "Content-Type: application/json" \
+ -d '{
+    "status":"complete"
+}' 
